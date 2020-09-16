139267889ea688d41d686aa2858e2c9454d0fab1
zereight <zereight@zereightui-MacBookPro.local>
2020-09-16 22:10:05 +0900

    svg

---
+++ src/svg.js
@@ -0,0 +1,37 @@
+// HTML DOM 선택
+const canvas = d3.select(".canvas");
+
+// append : 새로운 태그 추가
+// .attr : 속성지정
+const svg = canvas.append("svg").attr("height", 600).attr("width", 600);
+
+svg
+  .append("rect")
+  .attr("width", 200)
+  .attr("height", 100)
+  .attr("fill", "orange")
+  .attr("x", 20)
+  .attr("y", 20);
+
+svg
+  .append("circle")
+  .attr("r", 50)
+  .attr("cx", 300)
+  .attr("cy", 70)
+  .attr("fill", "purple");
+
+svg
+  .append("line")
+  .attr("x1", 370)
+  .attr("x2", 400)
+  .attr("y1", 20)
+  .attr("y2", 120)
+  .attr("stroke", "black");
+
+svg
+  .append("text") // text
+  .attr("x", 20)
+  .attr("y", 200)
+  .attr("fill", "green")
+  .text("Hello Text")
+  .style("font-family", "arial"); // style 적용
