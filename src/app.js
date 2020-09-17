6ca28ea2f913a59848f8252165650a860528a3ab
zereight <zereight@zereightui-MacBookPro.local>
2020-09-17 21:36:06 +0900

    enter()

--- src/app.js
+++ src/app.js
@@ -1,23 +1,33 @@
 const data = [
   {
     width: 200,
-    height: 200,
-    fill: "purple",
+    height: 100,
+    fill: "green",
+  },
+  {
+    width: 100,
+    height: 60,
+    fill: "orange",
+  },
+  {
+    width: 50,
+    height: 30,
+    fill: "skyblue",
   },
 ];
 
 const svg = d3.select("svg");
 
-const rect = svg
+const rects = svg.selectAll("rect").data(data);
+
+rects
+  .attr("width", (d, i, n) => d.width)
+  .attr("height", (d, i, n) => d.height)
+  .attr("fill", (d, i, n) => d.fill);
+
+rects
+  .enter()
   .append("rect")
-  .data(data)
-  .attr("width", (d) => {
-    // console.log(d);
-    return d.width;
-  })
-  .attr("height", (d) => {
-    return d.height;
-  })
-  .attr("fill", (d) => {
-    return d.fill;
-  });
+  .attr("width", (d, i, n) => d.width)
+  .attr("height", (d, i, n) => d.height)
+  .attr("fill", (d, i, n) => d.fill);
